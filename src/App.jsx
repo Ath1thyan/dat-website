4ff33ee2358b654db836890c556ff2bc03374dca
AravindKumar40m <aravindkumar40m@gmail.com>
2024-08-14 15:36:17 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="min-h-screen">
+      {/* navbar */}
+      <p>Dct website</p>
+      {/* who we are */}
+      {/* how we inovate */}
+      {/* our approach */}
+      {/* our values */}
+      {/* our polices */}
+      {/* the founder */}
+      {/* dct guild */}
+      {/* contact */}
+      {/* footer */}
+    </div>
+  );
+};
+
+export default App;
